f8786b644b5add2f2241f396bfdea6c918779758
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2016-09-18 10:02:35 +1000

    added some functionality for code comment detection and shebang transformation

--- plpy.pl
+++ plpy.pl
@@ -1,29 +1,72 @@
 #!/usr/bin/perl -w
 
-# written by andrewt@cse.unsw.edu.au September 2016
-# as a starting point for COMP2041/9041 assignment 
+# written by ksukumar@student.cse.unsw.edu.au September 2016
+# Assignment 1 for COMP2041/9041
 # http://cgi.cse.unsw.edu.au/~cs2041/assignments/plpy/
-# Replace this comment with your own header comment
+# Task to translate PERL Code to PYTHON Code
 
-while ($line = <>) {
-    if ($line =~ /^#!/ && $. == 1) {
-    
-        # translate #! line 
-        
-        print "#!/usr/local/bin/python3.5 -u";
-    } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
-    
-        # Blank & comment lines can be passed unchanged
-        
-        print $line;
-    } elsif ($line =~ /^\s*print\s*"(.*)\\n"[\s;]*$/) {
-        # Python's print adds a new-line character by default
-        # so we need to delete it from the Perl print statement
-        
-        print "print(\"$1\")\n";
-    } else {
-        # Lines we can't translate are turned into comments
-        
-        print "#$line\n";
+# Variable and Regex definitions
+
+$comment_code_regex = qr/(.*\"[^"]*\#[^"]*\"\;)(#.*)/;
+$commentline_regex = qr/([^"']*#[^"']*)/;
+$comment_inbegin_regex = qr/^\s*(#.*)/;
+
+sub handle_shebang
+{
+    my ($trans) = @_;
+    if ($line =~ /^#!/ && $. == 1) 
+    {
+        # translate #! line   
+        print "#!/usr/local/bin/python3.5 -u\n";
+        return 0;
     }
+    return 1;
+}
+sub handle_comment
+{
+    my ($trans) = @_;
+    # my (@array) =~ /$comment_code_regex/ ,$trans;
+    # print join @array,"\n";
+    my ($code, $comment);
+    if ($trans =~ /$comment_inbegin_regex/)
+    {
+        print "TRY 1";
+        print $1,"\n";
+    }
+    elsif ($trans =~ /$commentline_regex/)
+    {
+        print "TRY 2";
+        print $1,"\n";
+    }
+    elsif ($trans =~ /$comment_code_regex/)
+    {
+        $code = $1;
+        $comment = $2;
+        print "TRY 3";
+        print $code," ",$comment,"\n";
+    }
+}
+
+
+while ($line = <>) 
+{
+    chomp $line;
+    if (!handle_shebang($line))         #Handle Shebang line and move to the next line
+    {next;}
+    handle_comment($line);
+
+    # elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) 
+    # {
+    #     # Blank & comment lines can be passed unchanged
+    #     print $line;
+    # } elsif ($line =~ /^\s*print\s*"(.*)\\n"[\s;]*$/) {
+    #     # Python's print adds a new-line character by default
+    #     # so we need to delete it from the Perl print statement
+        
+    #     print "print(\"$1\")\n";
+    # } else {
+    #     # Lines we can't translate are turned into comments
+        
+    #     print "#$line\n";
+    # }
 }
---
+++ testcode.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+
+print "hello";	#prints hello
+print "\n";
+# while (<>)
+# {
+# 	print $_ if /^#/;
+# }
+$i = 0;
+while ($line = <>)
+{
+	$i++;
+	# print $i, " ";
+	if ($i %2 == 0)
+	{next;}				#	exit loop
+print $i, " ",$line;
+}
