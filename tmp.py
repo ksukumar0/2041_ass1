5a5515dcb12cfdf3223dbdd4753bd9e3c15a6012
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2016-10-01 22:09:23 +1000

    [Bug Fix] minor fix for join in print

--- tmp.py
+++ tmp.py
@@ -1,20 +1,6 @@
 #!/usr/local/bin/python3.5 -u
 import sys
-import fileinput
-# Test file to show some control statements
+# writen by andrewt@cse.unsw.edu.au as a COMP2041 example
+# implementation of /bin/echo
 
-input_text_array = fileinput.input()
-input_text_array=("").join(input_text_array)
-total= 0
-
-numbers= input_text_array.split('\D+')
-print ((",").join(numbers))
-
-for number in numbers :
-	if (number != ''):
-		total += number
-		n+=1
-
-if (numbers):
-	a= total/n
-	print("%s numbers: total %s mean is %s" %(n,total,a),end="")
+print( (' ').join(sys.argv[1:]), "\n")
